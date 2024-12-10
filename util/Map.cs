efd84697aadf051c24e872ef8c7a9e8ecb35fe91
Barion <ametrin.studios@gmail.com>
2024-12-10 08:33:52 +0100

    util

---
+++ util/Map.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+
+namespace AdventOfCode.Util;
+
+public sealed class CharMap(string map, int width) : IEnumerable<char>
+{
+    private readonly string map = map;
+    public int Width { get; } = width;
+    public int Height { get; } = map.Length / width;
+
+    public char this[int x, int y] => IsOnMap(x, y) ? map[y * Width + x] : '\0';
+    public char this[Vector2 pos] => this[pos.X, pos.Y];
+    public bool IsOnMap(int x, int y) => x >= 0 && y >= 0 && x < Width && y < Height;
+    public bool IsOnMap(Vector2 pos) => IsOnMap(pos.X, pos.Y);
+    public Vector2 IndexToPos(int index) => new(index % Width, index / Width);
+
+    public IEnumerator<char> GetEnumerator() => map.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => map.GetEnumerator();
+}
--- util/Vector2.cs
+++ util/Vector2.cs
@@ -2,7 +2,12 @@ namespace AdventOfCode.Util;
 
 public readonly record struct Vector2(int X, int Y)
 {
+    public static Vector2 Up { get; } = new(0, -1);
+    public static Vector2 Down { get; } = new(0, 1);
+    public static Vector2 Right { get; } = new(1, 0);
+    public static Vector2 Left { get; } = new(-1, 0);
     public int MagnitudeSqr => X * X + Y * Y;
+
     public static Vector2 operator +(Vector2 left, Vector2 right)
         => new(left.X + right.X, left.Y + right.Y);
     public static Vector2 operator -(Vector2 left, Vector2 right)
